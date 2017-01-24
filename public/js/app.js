7ad2d79c900c2a876c59afc7a050db1ca937f8e2
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-01-24 15:55:08 +0000

    add redux and dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import configureStore from './util/configureStore';
+import { setStore } from './util/storeAccessor';
+import { routes } from './routes';
+
 import '../styles/main.scss';
 
-console.log("Started Javascript application");
+const store = configureStore();
+
+setStore(store);
+
+render(
+    <Provider store={store}>
+      {routes}
+    </Provider>
+    , document.getElementById('react-mount'));
