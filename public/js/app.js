53fcd7f34996f6e6a6401aa887432e0efd802c2e
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-01-24 16:26:14 +0000

    fetch config and dispatch on init

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,28 @@ import { routes } from './routes';
 
 import '../styles/main.scss';
 
+function extractConfigFromPage() {
+
+    const configEl = document.getElementById('config');
+
+    if (!configEl) {
+        return {};
+    }
+
+    return JSON.parse(configEl.innerHTML);
+}
+
+
 const store = configureStore();
+const config = extractConfigFromPage();
+    console.log(config);
+
+// Send config to store on init
+store.dispatch({
+    type:       'CONFIG_RECEIVED',
+    config:     extractConfigFromPage(),
+    receivedAt: Date.now()
+});
 
 setStore(store);
 
