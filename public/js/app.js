1e3a7d41126b53f249492f36974e59f9187e27da
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-01-24 16:34:46 +0000

    remove storeAccessor

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,11 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import configureStore from './util/configureStore';
-import { setStore } from './util/storeAccessor';
 import { routes } from './routes';
 
 import '../styles/main.scss';
 
 const store = configureStore();
-
-setStore(store);
 
 render(
     <Provider store={store}>
