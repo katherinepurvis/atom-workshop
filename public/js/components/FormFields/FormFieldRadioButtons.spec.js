06c1fd3e1f0fe665014e61f053735a38a064aca3
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-02-02 12:25:45 +0000

    add tests for radio buttons

---
+++ public/js/components/FormFields/FormFieldRadioButtons.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import FormFieldRadioButtons from './FormFieldRadioButtons';
+import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
+
+let fieldLabel = 'test',
+    fieldName = 'test',
+    fieldValues = ['One', 'Two'];
+
+test('Should render', () => {
+  const component = renderer.create(
+    <FormFieldRadioButtons fieldLabel={fieldLabel} fieldName={fieldName} fieldValues={fieldValues} />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Should call update function on change', () => {
+
+  const updateFn = jest.fn();
+  const radio = shallow(
+    <FormFieldRadioButtons fieldLabel={fieldLabel} fieldName={fieldName} fieldValues={fieldValues} onUpdateField={updateFn}/>
+  );
+
+  radio.find('input').first().simulate('change');
+
+  expect(updateFn).toHaveBeenCalledTimes(1);
+});
