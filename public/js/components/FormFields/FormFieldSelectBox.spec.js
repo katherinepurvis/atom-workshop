11959805f55b2b19c6bb37edc5bc38f944ff7da5
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-02-02 12:27:42 +0000

    add tests for select box

---
+++ public/js/components/FormFields/FormFieldSelectBox.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import FormFieldSelectBox from './FormFieldSelectBox';
+import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
+
+let fieldLabel = 'test',
+    fieldName = 'test',
+    fieldValue= 'test',
+    selectValues = ['test', 'test1'];
+
+test('Should render', () => {
+  const component = renderer.create(
+    <FormFieldSelectBox fieldLabel={fieldLabel} fieldName={fieldName} fieldValue={fieldValue} selectValues={selectValues} />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Should call update function on change', () => {
+
+  const updateFn = jest.fn();
+  const select = shallow(
+    <FormFieldSelectBox fieldLabel={fieldLabel} fieldName={fieldName} fieldValue={fieldValue} selectValues={selectValues} onUpdateField={updateFn} />
+  );
+
+  select.find('select').simulate('change');
+
+  expect(updateFn).toHaveBeenCalledTimes(1);
+});
