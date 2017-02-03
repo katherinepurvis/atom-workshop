ed2b28e35803698a2dd75628d96d9b002498817a
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-02-03 16:53:06 +0000

    switch from reqwest and q to fetch and promises

--- public/js/services/AtomsApi.js
+++ public/js/services/AtomsApi.js
@@ -1,21 +1,29 @@
-import { pandaReqwest } from './pandaReqwest';
+import { pandaFetch } from './pandaFetch';
 import { getStore } from '../util/storeAccessor';
 
 export default {
 
   fetchAtom: (atomId, atomType) => {
-    return pandaReqwest({
-      url: `/api/preview/${atomType}/${atomId}`,
-      method: 'get'
-    });
+    return pandaFetch(
+      new Request(
+        `/api/preview/${atomType}/${atomId}`,
+        {
+          method: 'get'
+        }
+      )
+    );
   },
 
 
   createAtom: (atomType) => {
-    return pandaReqwest({
-      url: `/api/preview/${atomType}`,
-      method: 'post'
-    })
+    return pandaFetch(
+      new Request(
+        `/api/preview/${atomType}`,
+        {
+          method: 'post'
+        }
+      )
+    )
   }
 
 }
---
+++ public/js/services/pandaFetch.js
@@ -0,0 +1,39 @@
+import { reEstablishSession } from 'panda-session';
+import { getStore } from '../util/storeAccessor';
+
+function checkStatus(res) {
+  if (res.status >= 200 && res.status < 300) {
+    return res
+  } else {
+    console.log(res);
+    throw new Error(res)
+  }
+}
+
+export function pandaFetch(requestBody) {
+  return new Promise(function(resolve, reject) {
+    fetch(requestBody)
+        .then(checkStatus)
+        .then(res => resolve(res))
+        .catch(err => {
+          if (err.status == 419) {
+            const store = getStore();
+            var reauthUrl = store.getState().config.reauthUrl;
+
+            reEstablishSession(reauthUrl, 5000).then(
+                res => {
+                  fetch(requestBody)
+                  .then(checkStatus)
+                  .then(res => resolve(res))
+                  .catch(err => reject(err));
+                },
+                error => {
+                  throw error;
+                });
+
+          } else {
+            reject(err)
+          }
+        });
+  });
+}
