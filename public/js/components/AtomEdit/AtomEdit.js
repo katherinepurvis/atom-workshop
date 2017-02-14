6b8f6b4d53b296d574344c677a089a481470604d
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-02-14 13:27:09 +0000

    put atom in state on mount

--- public/js/components/AtomEdit/AtomEdit.js
+++ public/js/components/AtomEdit/AtomEdit.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 
-export class AtomEdit extends React.Component {
+class AtomEdit extends React.Component {
+
+  static propTypes = {
+    routeParams: PropTypes.shape({
+      atomType: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired
+    }).isRequired,
+    atomActions: PropTypes.shape({
+      getAtom: PropTypes.func.isRequired
+    }).isRequired,
+    atom: PropTypes.object
+  }
+
+  componentWillMount() {
+    this.props.atomActions.getAtom(this.props.routeParams.atomType, this.props.routeParams.id);
+  }
+
   render () {
+
     return (
-      <div>Atom Edit</div>
+      <p>{this.props.atom || {}}</p>
     );
   }
 }
+
+//REDUX CONNECTIONS
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as getAtomActions from '../../actions/AtomActions/getAtom.js';
+
+function mapStateToProps(state) {
+  return {
+    config: state.config,
+    atom: state.atom
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    atomActions: bindActionCreators(Object.assign({}, getAtomActions), dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AtomEdit);
