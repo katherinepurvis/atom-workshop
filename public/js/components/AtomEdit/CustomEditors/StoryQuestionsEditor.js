55eec96c1658ad9fa61292a51063a4067f5bd4ca
Christopher Lloyd <christopher.lloyd@theguardian.com>
2017-03-01 17:08:02 +0000

    Story Questions Editor

--- public/js/components/AtomEdit/CustomEditors/StoryQuestionsEditor.js
+++ public/js/components/AtomEdit/CustomEditors/StoryQuestionsEditor.js
@@ -1,7 +1,11 @@
 import React, { PropTypes } from 'react';
 import FormFieldTagPicker from '../../FormFields/FormFieldTagPicker';
+import FormFieldTextInput from '../../FormFields/FormFieldTextInput';
+import FormFieldArrayWrapper from '../../FormFields/FormFieldArrayWrapper';
+import {StoryQuestionsQuestionSet} from './StoryQuestionFields/QuestionSet';
 import {ManagedField, ManagedForm} from '../../ManagedEditor';
 import {atomPropType} from '../../../constants/atomPropType';
+
 
 export class StoryQuestionsEditor extends React.Component {
 
@@ -12,11 +16,26 @@ export class StoryQuestionsEditor extends React.Component {
 
   render () {
     return (
+      <div className="form">
         <ManagedForm data={this.props.atom} updateData={this.props.onUpdate}>
+          <ManagedField fieldLocation="data.storyquestions.title" name="Public Title" isRequired={true}>
+            <FormFieldTextInput/>
+          </ManagedField>
           <ManagedField fieldLocation="data.storyquestions.relatedStoryId" name="Related Tag" isRequired={true}>
             <FormFieldTagPicker/>
           </ManagedField>
+          <ManagedField fieldLocation="data.storyquestions.editorialQuestions" name="Editorial Questions">
+            <FormFieldArrayWrapper>
+              <StoryQuestionsQuestionSet />
+            </FormFieldArrayWrapper>
+          </ManagedField>
+          <ManagedField fieldLocation="data.storyquestions.userQuestions" name="User Questions">
+            <FormFieldArrayWrapper>
+              <StoryQuestionsQuestionSet />
+            </FormFieldArrayWrapper>
+          </ManagedField>
         </ManagedForm>
+      </div>
     );
   }
 }
