62bff27a1f53336c3a109b0d9fa7e91704c92c2a
Christopher Lloyd <christopher.lloyd@theguardian.com>
2017-03-01 10:41:11 +0000

    Move CAPI logic out into separate service

--- app/controllers/Support.scala
+++ app/controllers/Support.scala
@@ -2,34 +2,15 @@ package controllers
 
 import play.api.libs.ws.WSClient
 import play.api.mvc.Controller
-import com.squareup.okhttp.{Credentials, OkHttpClient, Request}
-import config.Config
-import java.util.concurrent.TimeUnit
-
-
+import services.ContentApi
 
 class Support(val wsClient: WSClient) extends Controller with PanDomainAuthActions {
 
   def previewCapiProxy(path: String) = APIAuthAction { request =>
-    val httpClient = new OkHttpClient()
-    httpClient.setConnectTimeout(5, TimeUnit.SECONDS)
 
-    val resp = for {
-      capiPreviewUser <- Config.capiUsername
-      capiPreviewPassword <- Config.capiPassword
-      capiUrl <- Config.capiPreviewUrl
-    } yield {
-      val url = s"$capiUrl/$path?${request.rawQueryString}"
-      
-      val req = new Request.Builder()
-        .url(url)
-        .header("Authorization", Credentials.basic(capiPreviewUser, capiPreviewPassword))
-        .build
+    val capiPath = s"$path?${request.rawQueryString}"
 
-      httpClient.newCall(req).execute
-    }
-
-    resp match {
+    ContentApi.requestPreviewPath(capiPath) match {
       case None =>
         InternalServerError("Could not construct CAPI request, check config has capi information")
       case Some(r) if r.code() == 200 =>
