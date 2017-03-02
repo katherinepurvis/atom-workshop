601a12e823993db60fe1bb4dbc2229b8d4cc8da4
Christopher Lloyd <christopher.lloyd@theguardian.com>
2017-03-02 12:05:45 +0000

    lint error

--- public/js/components/AtomRoot/AtomRoot.js
+++ public/js/components/AtomRoot/AtomRoot.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
 import AtomEmbed from '../AtomEmbed/AtomEmbed';
 
 import {atomPropType} from '../../constants/atomPropType.js';
