1f5048e82c70532e8d58402d55092b332d0b6e86
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-03-02 17:24:24 +0000

    add atom usages and hit capi for the pages

--- public/js/services/capi.js
+++ public/js/services/capi.js
@@ -3,34 +3,58 @@ import { uriEncodeParams } from '../util/uriEncodeParams';
 
 export default {
 
-    searchTags: function(searchText) {
-        return pandaFetch(
-            `/support/previewCapi/tags?q=${searchText}`,
-            {
-                method: 'get',
-                credentials: 'same-origin'
-            }
-        )
-        .then((res) => res.json())
-        .then((json) => Promise.resolve(json.response.results));
-    },
+  searchTags: function(searchText) {
+    return pandaFetch(
+      `/support/previewCapi/tags?q=${searchText}`,
+      {
+        method: 'get',
+        credentials: 'same-origin'
+      }
+    )
+    .then((res) => res.json())
+    .then((json) => Promise.resolve(json.response.results));
+  },
 
-    searchAtoms: function(query) {
-        console.log(`/support/previewCapi/atoms?${uriEncodeParams(query)}`);
-        return pandaFetch(
-            `/support/previewCapi/atoms?${uriEncodeParams(query)}`,
-            {
-                method: 'get',
-                credentials: 'same-origin'
-            }
-        )
-        .then((res) => res.json())
-        .then((json) => {
-            console.log(json)
-            return Promise.resolve(json.response.results)});
-    }
+  searchAtoms: function(query) {
+    return pandaFetch(
+      `/support/previewCapi/atoms?${uriEncodeParams(query)}`,
+      {
+        method: 'get',
+        credentials: 'same-origin'
+      }
+    )
+    .then((res) => res.json())
+    .then((json) => {
+      return Promise.resolve(json.response.results);
+    });
+  },
 
-}
+  getAtomUsages: function(atomId, atomType) {
+    return pandaFetch(
+      `/support/previewCapi/atom/${atomType}/${atomId}/usage`,
+      {
+        method: 'get',
+        credentials: 'same-origin'
+      }
+    )
+    .then((res) => res.json())
+    .then((json) => {
+      return Promise.resolve(json.response.results);
+    });
+  },
 
+  getByPath: function(path) {
+    return pandaFetch(
+      `/support/previewCapi/${path}?show-fields=all`,
+      {
+        method: 'get',
+        credentials: 'same-origin'
+      }
+    )
+    .then((res) => res.json())
+    .then((json) => {
+      return Promise.resolve(json.response.results);
+    });
+  }
 
-
+};
