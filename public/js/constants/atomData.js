127c07301cc8a53a81e77937ffb6daff5937d882
Christopher Lloyd <christopher.lloyd@theguardian.com>
2017-03-07 17:14:48 +0000

    Add linking out to external atom creation tools

--- public/js/constants/atomData.js
+++ public/js/constants/atomData.js
@@ -3,11 +3,14 @@
 //fullName - The human readable name (Don't include "Atom")
 //description - A brief description of what they are and when you use them
 //editorUri - a lodash template that will be passed gutoolsDomain, atomId, atomType
-//createUri - the url to be taken to for creation.
+//createUri - a lodash template that will be passed gutoolsDomain.
 
 
 import {PropTypes} from 'react';
 import _template from 'lodash/fp/template';
+import {logInfo} from '../util/logger';
+import { getStore } from '../util/storeAccessor';
+
 
 export const cta = {
   type: "cta",
@@ -19,7 +22,8 @@ export const explainer = {
   type: "explainer",
   fullName: "Explainer Text",
   description: "Provide extra context to any article with an Explainer Text Box",
-  editorUri: _template("https://explainers.${gutoolsDomain}/explain/${atomId}")
+  editorUri: _template("https://explainers.${gutoolsDomain}/explainer/${atomId}"),
+  createUri: _template("https://explainers.${gutoolsDomain}")
 };
 
 export const recipe = {
@@ -38,21 +42,44 @@ export const quiz = {
   type: "quiz",
   fullName: "Quiz",
   description: "Questions and Answer format, allowing both knowledge and personality type quizzes",
-  editorUri: _template("https://quizzes.${gutoolsDomain}/quiz/${atomId}")
+  editorUri: _template("https://quizzes.${gutoolsDomain}/quiz/${atomId}"),
+  createUri: _template("https://quizzes.${gutoolsDomain}")
 };
 
 export const media = {
   type: "media",
   fullName: "Video",
   description: "A Guardian produced video, with rich tracking and thumbnail. Generally hosted on YouTube",
-  editorUri: _template("https://video.${gutoolsDomain}/videos/${atomId}")
+  editorUri: _template("https://video.${gutoolsDomain}/videos/${atomId}"),
+  createUri: _template("https://video.${gutoolsDomain}/videos/create")
 };
 
 export const allAtomTypes = [cta, explainer, recipe, storyQuestions, quiz, media];
 export const workshopEditableAtomTypes = [cta, storyQuestions, recipe];
 
-export function getAtomByType(type) {
-  return allAtomTypes.filter((atomData) => atomData.type.toLowerCase() === type.toLowerCase())[0];
+export function getAtomByType(typeString) {
+  return allAtomTypes.find((atomData) => atomData.type.toLowerCase() === typeString.toLowerCase());
+}
+
+export function isAtomTypeEditable(typeString) {
+  const matchingType = workshopEditableAtomTypes.find((atomData) => atomData.type.toLowerCase() === typeString.toLowerCase());
+  return !!matchingType;
+}
+
+export function getCreateUrlFromAtomType(atomType) {
+  if (isAtomTypeEditable(atomType.type)) {
+    return `/create/${atomType.type}`;
+  } else if (!atomType.createUri) {
+    logInfo(`Create URI requested for type ${atomType.type}, none found`);
+    return;
+  }
+
+  const store = getStore();
+  const state = store.getState();
+
+  return atomType.createUri({
+    gutoolsDomain: state.config.atomEditorGutoolsDomain
+  });
 }
 
 //Keep this together with the data
