0ae7a80d7b8e84dea36e7e0490c700a429f68da8
Paul Roberts <paul.roberts@guardian.co.uk>
2017-04-05 10:15:51 +0100

    actually do stuff

--- snapper/src/main/scala/Snapper.scala
+++ snapper/src/main/scala/Snapper.scala
@@ -1,5 +1,6 @@
 package com.gu.atomworkshop.snapper
 
+import scala.collection.JavaConversions._
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.services.dynamodbv2._
 import model._
@@ -13,22 +14,74 @@ import model._
  * changes.
  */
 
-object Snapper extends App {
-  /* these are the configuration settings that are just being
-   * hard-coded here because I don't want to include a proper
-   * configuration tool right now. */
-  val dynamoTable = "atom-workshop-preview-DEV"
-  // this comes from the AWS console, after creating the stream for a given dynamo table.
-  val streamArn = "arn:aws:dynamodb:eu-west-1:743583969668:table/atom-workshop-preview-DEV/stream/2017-04-04T08:12:21.600"
+class Snapper(streamArn: String) {
   val authProvider = new ProfileCredentialsProvider("composer")
-
   val streamsClient = {
     val cl = new AmazonDynamoDBStreamsClient(authProvider)
     cl.setEndpoint("https://streams.dynamodb.eu-west-1.amazonaws.com")
     cl
   }
-  val streamDetails = streamsClient.describeStream(
-    new DescribeStreamRequest().withStreamArn(streamArn)
-  )
-  println(streamDetails)
+
+  def getShards = {
+    val streamDetailsResult = streamsClient.describeStream(
+      new DescribeStreamRequest().withStreamArn(streamArn)
+    )
+    streamDetailsResult.getStreamDescription.getShards
+  }
+
+  def newShardIterator(shardId: String) = {
+    streamsClient.getShardIterator(
+      new GetShardIteratorRequest()
+        .withStreamArn(streamArn)
+        .withShardId(shardId)
+        .withShardIteratorType(ShardIteratorType.TRIM_HORIZON)
+    ).getShardIterator
+  }
+
+  def process(iterators: Map[String, String]): Map[String, String] = {
+    (for(shard <- getShards) yield {
+      val shardId = shard.getShardId
+      println(s"Processing shard: $shardId")
+      val it = iterators.get(shardId) match {
+        case Some(it) =>
+          println(s"\tfound iterator: $it")
+          it
+        case None =>
+          val it = newShardIterator(shardId)
+          println(s"\trequested new iterator: $it")
+          it
+      }
+      processIterator(it) match {
+        case null => None
+        case nextIt => Some(shardId -> nextIt)
+      }
+    }).flatten.toMap
+  }
+
+  def processIterator(it: String): String = {
+  val recordsResult = streamsClient.getRecords(
+      new GetRecordsRequest().withShardIterator(it)
+    )
+    val records = recordsResult.getRecords
+    println(s"\tRecords: ${records.length}")
+    recordsResult.getNextShardIterator()
+  }
+
+}
+
+object SnapperTest {
+  val snapper = new Snapper("arn:aws:dynamodb:eu-west-1:743583969668:table/atom-workshop-preview-DEV/stream/2017-04-04T08:12:21.600")
+
+  @annotation.tailrec
+  def run(delay: Long = 5000, iterators: Map[String, String] = Map.empty): Unit = {
+    println(s"Sleeping for ${delay}ms")
+    Thread.sleep(delay)
+    val nextIterators = snapper.process(iterators)
+    run(delay, nextIterators)
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    run()
+  }
 }
