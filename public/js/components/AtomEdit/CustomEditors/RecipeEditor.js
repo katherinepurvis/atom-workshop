7128f8aa66fd0f092dd3d4a3bfc480d5c03516a4
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-05-23 11:46:29 +0100

    remove stray tag (#126)

--- public/js/components/AtomEdit/CustomEditors/RecipeEditor.js
+++ public/js/components/AtomEdit/CustomEditors/RecipeEditor.js
@@ -27,11 +27,12 @@ export class RecipeEditor extends React.Component {
   render () {
     return (
 
-      <ManagedForm data={this.props.atom} updateData={this.props.onUpdate} onFormErrorsUpdate={this.props.onFormErrorsUpdate} formName="recipeEditor">>
+      <ManagedForm data={this.props.atom} updateData={this.props.onUpdate} onFormErrorsUpdate={this.props.onFormErrorsUpdate} formName="recipeEditor">
 
         <ManagedField fieldLocation="data.recipe.tags.cuisine" name="Cuisine" isRequired={true}>
           <FormFieldMultiSelect selectValues={recipeData.cuisineList}/>
         </ManagedField>
+
         <ManagedField fieldLocation="data.recipe.tags.category" name="Category" isRequired={true}>
           <FormFieldMultiSelect selectValues={recipeData.categoryList}/>
         </ManagedField>
@@ -55,19 +56,23 @@ export class RecipeEditor extends React.Component {
         <ManagedField fieldLocation="data.recipe.serves" name="Serving Information" isRequired={true}>
           <RecipeServings />
         </ManagedField>
+
         <ManagedField fieldLocation="data.recipe.ingredientsLists" name="Ingredient Lists">
           <FormFieldArrayWrapper>
             <IngredientList onFormErrorsUpdate={this.props.onFormErrorsUpdate} />
           </FormFieldArrayWrapper>
         </ManagedField>
+
         <ManagedField fieldLocation="data.recipe.steps" name="Steps" isRequired={true}>
           <FormFieldArrayWrapper numbered={true} fieldClass="form__group form__group--flex">
             <FormFieldTextInput />
           </FormFieldArrayWrapper>
         </ManagedField>
+
         <ManagedField fieldLocation="data.recipe.credits" name="Chefs" isRequired={true}>
           <FormFieldMultiSelect selectValues={recipeData.chefs}/>
         </ManagedField>
+        
         <ManagedField fieldLocation="data.recipe.images" name="Images" isRequired={true}>
           <FormFieldArrayWrapper>
             <FormFieldImageSelect gridUrl={this.props.config.gridUrl}/>
