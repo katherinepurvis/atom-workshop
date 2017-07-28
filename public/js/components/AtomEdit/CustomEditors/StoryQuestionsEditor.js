ae5759c5a61160f54dc3a30e8c67df7e4832c277
tomrf1 <tom.forbes@theguardian.com>
2017-07-28 11:58:49 +0100

    Storyquestions closeDate field

--- public/js/components/AtomEdit/CustomEditors/StoryQuestionsEditor.js
+++ public/js/components/AtomEdit/CustomEditors/StoryQuestionsEditor.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import FormFieldTagPicker from '../../FormFields/FormFieldTagPicker';
 import FormFieldTextInput from '../../FormFields/FormFieldTextInput';
+import FormFieldCheckbox from '../../FormFields/FormFieldCheckbox';
 import {StoryQuestionsQuestionSet} from './StoryQuestionFields/QuestionSet';
 import {ManagedField, ManagedForm} from '../../ManagedEditor';
 import {atomPropType} from '../../../constants/atomPropType';
@@ -12,12 +13,42 @@ export class StoryQuestionsEditor extends React.Component {
     atom: atomPropType.isRequired,
     onUpdate: PropTypes.func.isRequired,
     onFormErrorsUpdate: PropTypes.func
-  }
+  };
+
+  state = {
+    isClosed: this.props.atom.data.storyquestions.closeDate !== undefined && Date.now() >= this.props.atom.data.storyquestions.closeDate
+  };
+
+  //The model has a DateTime field, but for now the UI exposes this as a checkbox - closed or not
+  updateClose = (isClosed) => {
+    this.setState({isClosed: isClosed});
+
+    if (isClosed) {
+      const closed = Object.assign({}, this.props.atom, {
+        data: Object.assign({}, this.props.atom.data, {
+          storyquestions: Object.assign({}, this.props.atom.data.storyquestions, {
+            closeDate: Date.now()
+          })
+        })
+      });
+      this.props.onUpdate(closed);
+    } else {
+      const notClosed = Object.assign({}, this.props.atom, {
+        data: Object.assign({}, this.props.atom.data, {
+          storyquestions: Object.assign({}, this.props.atom.data.storyquestions, {
+            closeDate: undefined
+          })
+        })
+      });
+      this.props.onUpdate(notClosed);
+    }
+  };
 
   render () {
     return (
       <div className="form">
         <ManagedForm data={this.props.atom} updateData={this.props.onUpdate} onFormErrorsUpdate={this.props.onFormErrorsUpdate} formName="storyquestionsEditor">
+          <FormFieldCheckbox fieldName="Closed" fieldValue={this.state.isClosed} onUpdateField={this.updateClose.bind(this)} />
           <ManagedField fieldLocation="data.storyquestions.title" name="Public Title" isRequired={true}>
             <FormFieldTextInput/>
           </ManagedField>
