20172b3df6564bf2d617c924920f0d8545476b07
Anne Byrne <anne.byrne@guardian.co.uk>
2017-09-18 11:03:44 +0100

    Fix to use atom title as default proposer title

--- public/js/components/FormFields/FormFieldTextInput.js
+++ public/js/components/FormFields/FormFieldTextInput.js
@@ -20,10 +20,18 @@ export default class FormFieldTextInput extends React.Component {
   }
 
   render() {
+
     return (
         <div className={this.props.formRowClass || "form__row"}>
           {this.props.fieldLabel ? <label htmlFor={this.props.fieldName} className="form__label">{this.props.fieldLabel}</label> : false}
-          <input type="text" className={"form__field " + (this.props.fieldErrors && this.props.fieldErrors.length ? "form__field--error" : "")}  id={this.props.fieldName} placeholder={this.props.fieldPlaceholder || ''} onChange={this.onUpdate}  value={this.props.fieldValue || ""}/>
+          <input
+              type="text"
+              className={"form__field " + (this.props.fieldErrors && this.props.fieldErrors.length ? "form__field--error" : "")}
+              id={this.props.fieldName}
+              placeholder={this.props.fieldPlaceholder || ''}
+              onChange={this.onUpdate}
+              value={this.props.fieldValue || ""}
+          />
           <ShowErrors errors={this.props.fieldErrors}/>
         </div>
 
