9d0ed689327372588a16a72d4e9dd50053b45892
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2017-09-28 09:39:52 +0100

    add api for interacting with workflow

---
+++ public/js/util/getProductionOffice.js
@@ -0,0 +1,34 @@
+import moment from 'moment';
+
+function isBetween({value, lower, upper}) {
+  return value >= lower && value <= upper;
+}
+
+function getTimezoneOffset() {
+  const offset = moment().utcOffset();
+
+  if (isBetween({value: offset, lower: 480, upper: 660})) {
+    return 'SYD';
+  }
+
+  if (isBetween({value: offset, lower: -560, upper: -400})) {
+    return 'SFO';
+  }
+
+  if (isBetween({value: offset, lower: -400, upper: -240})) {
+    return 'NYC';
+  }
+
+  return 'LON';
+}
+
+export default function getProductionOffice() {
+  const timezoneProductionOfficeMap = {
+    SYD: 'AUS',
+    SFO: 'US',
+    NYC: 'US',
+    LON: 'UK'
+  };
+
+  return timezoneProductionOfficeMap[getTimezoneOffset()];
+}
