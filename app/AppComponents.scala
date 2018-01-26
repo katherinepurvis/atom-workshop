4a9d280ad2b676f1c58d769c75645b031b7cbe28
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-01-26 16:29:26 +0000

    Add migration controller

--- app/AppComponents.scala
+++ app/AppComponents.scala
@@ -15,9 +15,10 @@ class AppComponents(context: Context)
 
   val logger = new LogConfig
 
-  lazy val router = new Routes(httpErrorHandler, appController, healthcheckController, loginController, assets, supportController, reindex, atomActionsController)
+  lazy val router = new Routes(httpErrorHandler, appController, healthcheckController, loginController, assets, supportController, reindex, atomActionsController, migrationController)
   lazy val assets = new controllers.Assets(httpErrorHandler)
   lazy val appController = new controllers.App(wsClient, atomWorkshopDB, permissions)
+  lazy val migrationController = new controllers.Migration(wsClient, atomWorkshopDB)
   lazy val loginController = new controllers.Login(wsClient)
   lazy val healthcheckController = new controllers.Healthcheck()
   lazy val supportController = new controllers.Support(wsClient)
