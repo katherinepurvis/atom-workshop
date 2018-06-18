76deca39cf9fa73b252170af2104a4cf0e5a3ec9
Michael Barton <michael.barton@guardian.co.uk>
2018-06-18 16:56:16 +0100

    Default query parameters

--- public/js/reducers/queryParamsReducer.js
+++ public/js/reducers/queryParamsReducer.js
@@ -1,8 +1,16 @@
-export default function queryParams(state = {}, action) {
+import _isEmpty from 'lodash/isEmpty';
+import {searchParams} from '../constants/queryParams';
+
+export default function queryParams(state = searchParams, action) {
   switch (action.type) {
 
-    case 'QUERYPARAMS_UPDATE':
-      return action.queryParams;
+    case 'QUERYPARAMS_UPDATE': {
+      if(_isEmpty(action.queryParams)) {
+        return searchParams;
+      } else {
+        return action.queryParams;
+      }
+    }
 
     default:
       return state;
