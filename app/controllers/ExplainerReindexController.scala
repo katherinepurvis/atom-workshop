3a97e23b3425f7eb9871416e6890991745911170
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-04-29 16:04:54 +0100

    Use api key

--- app/controllers/ExplainerReindexController.scala
+++ app/controllers/ExplainerReindexController.scala
@@ -5,7 +5,7 @@ import com.gu.atom.publish.{AtomReindexer, PreviewAtomReindexer, PublishedAtomRe
 import db.ExplainerDBAPI
 import play.api.Configuration
 import play.api.libs.ws.WSClient
-import play.api.mvc.{ Action, AnyContent, Controller, Result }
+import play.api.mvc.{ Action, ActionBuilder, AnyContent, Controller, Result, Request }
 import com.gu.contentatom.thrift.{ContentAtomEvent, EventType}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
@@ -20,7 +20,19 @@ class ExplainerReindexController(
   config: Configuration
 )(implicit ec: ExecutionContext) extends Controller with PanDomainAuthActions {
 
-  def reindex(stack: String): Action[AnyContent] = AuthAction.async { req => 
+  // Copy-pasted from the atom-maker library
+  object ApiKeyAction extends ActionBuilder[Request] {
+    lazy val apiKey = config.getString("reindexApiKey").get
+
+    def invokeBlock[A](request: Request[A], block: (Request[A] => Future[Result])) = {
+      if(request.getQueryString("api").contains(apiKey))
+        block(request)
+      else
+        Future.successful(Unauthorized(""))
+    }
+  }
+
+  def reindex(stack: String): Action[AnyContent] = ApiKeyAction.async { req => 
     stack match {
       case "preview" => run(previewDataStore, previewReindexer) map displayResult recover displayError
       case "published" => run(publishedDataStore, publishedReindexer) map displayResult recover displayError
