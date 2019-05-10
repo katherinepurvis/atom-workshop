476e5596c42e4f25651f7e8525f53b1602303dc3
Laura buns <lawr@lauragonzalez.cc>
2019-05-10 15:40:25 +0100

    use spaces

--- public/js/util/wordLimits.js
+++ public/js/util/wordLimits.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export const wordLimits = {
-	default: 400,
-	qanda: 250,
+  default: 400,
+  qanda: 250,
 };
 
 export const tooLongMsg = wordLimit => (
-	<div>
-		Heads up.{' '}
-		<strong>You've exceeded the {wordLimit} word limit for this field.</strong>{' '}
-		You can still publish it, but bear in mind that atoms should be concise.
-	</div>
+  <div>
+    Heads up.{" "}
+    <strong>You've exceeded the {wordLimit} word limit for this field.</strong>{" "}
+    You can still publish it, but bear in mind that atoms should be concise.
+  </div>
 );
