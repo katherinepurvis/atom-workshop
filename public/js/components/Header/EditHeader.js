6c5b460599e2b1de1c92cf7c64de16fb23dd5233
Laura buns <lawr@lauragonzalez.cc>
2019-05-13 14:07:03 +0100

    error dropdown

--- public/js/components/Header/EditHeader.js
+++ public/js/components/Header/EditHeader.js
@@ -8,9 +8,9 @@ import { saveStateVals } from '../../constants/saveStateVals';
 import distanceInWords from 'date-fns/distance_in_words';
 import DeleteAtom from './DeleteAtom';
 import ShowAllErrors from '../Utilities/ShowAllErrors';
+import HoverExpander from '../Utilities/HoverExpander';
 
 class EditHeader extends React.Component {
-
   static propTypes = {
     atom: atomPropType,
     presence: PropTypes.bool,
@@ -18,111 +18,136 @@ class EditHeader extends React.Component {
     atomActions: PropTypes.shape({
       publishAtom: PropTypes.func.isRequired,
       takeDownAtom: PropTypes.func.isRequired,
-      deleteAtom: PropTypes.func.isRequired
+      deleteAtom: PropTypes.func.isRequired,
     }).isRequired,
     config: PropTypes.shape({
       isEmbedded: PropTypes.bool.isRequired,
-      embeddedMode: PropTypes.string
-    })
-  }
+      embeddedMode: PropTypes.string,
+    }),
+  };
 
   publishAtom = () => {
     this.props.atomActions.publishAtom(this.props.atom);
-  }
+  };
 
   takeDownAtom = () => {
     this.props.atomActions.takeDownAtom(this.props.atom);
-  }
+  };
 
   isEditor = () => {
-    return location.pathname.search(new RegExp('\/atoms\/.*\/.*\/edit', 'g')) >= 0;
-  }
+    return location.pathname.search(new RegExp('/atoms/.*/.*/edit', 'g')) >= 0;
+  };
 
   timeSinceLastModified = () => {
-    if (this.props.atom.contentChangeDetails.created || this.props.atom.contentChangeDetails.lastModified) {
+    if (
+      this.props.atom.contentChangeDetails.created ||
+      this.props.atom.contentChangeDetails.lastModified
+    ) {
       const dateNow = Date.now();
-      const lastModified = this.props.atom.contentChangeDetails.lastModified ? this.props.atom.contentChangeDetails.lastModified.date : this.props.atom.contentChangeDetails.created.date;
+      const lastModified = this.props.atom.contentChangeDetails.lastModified
+        ? this.props.atom.contentChangeDetails.lastModified.date
+        : this.props.atom.contentChangeDetails.created.date;
       return distanceInWords(dateNow, lastModified, { addSuffix: true });
     }
     return false;
-  }
+  };
 
   renderSaveState = () => {
-
     if (this.props.saveState.saving === saveStateVals.inprogress) {
-      return (
-        <span className="loader save-state__loader">Saving</span>
-      );
+      return <span className="loader save-state__loader">Saving</span>;
     }
     return (
-      <span><span className="save-state">Saved</span> {this.timeSinceLastModified()}</span>
+      <span>
+        <span className="save-state">Saved</span> {this.timeSinceLastModified()}
+      </span>
     );
-  }
+  };
 
   renderPublishedState = () => {
     const atomPublishState = publishState(this.props.atom);
     return (
-      <span className={`publish-state publish-state--${atomPublishState.id}`}>{atomPublishState.text}</span>
+      <span className={`publish-state publish-state--${atomPublishState.id}`}>
+        {atomPublishState.text}
+      </span>
     );
-  }
+  };
 
-  renderTakeDownButton = (atomPublishState) => {
+  renderTakeDownButton = atomPublishState => {
     if (atomPublishState.id !== 'draft') {
       return (
-        <button type="button" disabled={atomPublishState.id === 'taken-down'} onClick={this.takeDownAtom} className="btn btn--red btn--margin">Take down</button>
+        <button
+          type="button"
+          disabled={atomPublishState.id === 'taken-down'}
+          onClick={this.takeDownAtom}
+          className="btn btn--red btn--margin"
+        >
+          Take down
+        </button>
       );
     }
     return false;
-  }
+  };
 
   renderHeaderRight = () => {
     const atomPublishState = publishState(this.props.atom);
 
     return (
       <div className="toolbar__container">
-        {this.props.presence ? <PresenceIndicator presence={this.props.presence} /> : false}
-        <button disabled={atomPublishState.id === 'published'} type="button" onClick={this.publishAtom} className="btn btn--green btn--margin">Publish</button>
+        {this.props.presence ? (
+          <PresenceIndicator presence={this.props.presence} />
+        ) : (
+          false
+        )}
 
-        <ShowAllErrors />
+        <HoverExpander
+          proxy={
+            <button
+              disabled={atomPublishState.id === 'published'}
+              type="button"
+              onClick={this.publishAtom}
+              className="btn btn--green btn--margin"
+            >
+              Publish
+            </button>
+          }
+        >
+          <ShowAllErrors />
+        </HoverExpander>
 
         {this.renderTakeDownButton(atomPublishState)}
 
-        <DeleteAtom atom={this.props.atom} deleteAtom={this.props.atomActions.deleteAtom} />
+        <DeleteAtom
+          atom={this.props.atom}
+          deleteAtom={this.props.atomActions.deleteAtom}
+        />
       </div>
     );
-  }
+  };
 
   renderAtomStates = () => {
     return (
       <div className="toolbar__container">
-        <div className="toolbar__item">
-          {this.renderPublishedState()}
-        </div>
-        <div className="toolbar__item">
-          {this.renderSaveState()}
-        </div>
+        <div className="toolbar__item">{this.renderPublishedState()}</div>
+        <div className="toolbar__item">{this.renderSaveState()}</div>
       </div>
     );
-  }
+  };
 
   renderEmbeddedheader = () => {
-    if (this.props.config.embeddedMode === "edit") {
+    if (this.props.config.embeddedMode === 'edit') {
       return false;
     }
 
     return (
-      <Link to="/find" className="toolbar__button">Back to Atom Search</Link>
+      <Link to="/find" className="toolbar__button">
+        Back to Atom Search
+      </Link>
     );
-  }
+  };
 
   render() {
-
     if (this.props.config.isEmbedded) {
-      return (
-        <div>
-          {this.renderEmbeddedheader()}
-        </div>
-      );
+      return <div>{this.renderEmbeddedheader()}</div>;
     }
 
     if (this.props.atom) {
@@ -149,14 +174,25 @@ function mapStateToProps(state) {
   return {
     saveState: state.saveState,
     config: state.config,
-    presence: state.presence
+    presence: state.presence,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    atomActions: bindActionCreators(Object.assign({}, publishAtomActions, takeDownAtomActions, deleteAtomActions), dispatch)
+    atomActions: bindActionCreators(
+      Object.assign(
+        {},
+        publishAtomActions,
+        takeDownAtomActions,
+        deleteAtomActions
+      ),
+      dispatch
+    ),
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(EditHeader);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditHeader);
--- public/js/components/Utilities/ShowAllErrors.js
+++ public/js/components/Utilities/ShowAllErrors.js
@@ -1,38 +1,39 @@
 import React, { PropTypes } from 'react';
 
 class ShowAllErrors extends React.Component {
-	static propTypes = {
-		formErrors: PropTypes.array,
-	};
+  static propTypes = {
+    formErrors: PropTypes.array,
+  };
 
-	render() {
-		const { formErrors } = this.props;
-		return (
-			<div className="list">
-				{formErrors.map(([_, fields]) =>
-					fields.map(([field, errors]) =>
-						errors.map(({ title, message }) => (
-							<div className="list__item">
-								{field} {title} err {message}
-							</div>
-						))
-					)
-				)}
-			</div>
-		);
-	}
+  render() {
+    const { formErrors } = this.props;
+    return (
+      <section className="list">
+        {formErrors.map(([_, fields]) =>
+          fields.map(([field, errors]) =>
+            errors.map(({ title, message }) => (
+              <div className="list__item list__item--error">
+                <div className="list__item__title">Error: {field}</div>
+                <p className="list__item__body">{message}</p>
+              </div>
+            ))
+          )
+        )}
+      </section>
+    );
+  }
 }
 
 //REDUX CONNECTIONS
 import { connect } from 'react-redux';
 
 function mapStateToProps(state) {
-	return {
-		formErrors: Object.entries(state.formErrors).map(([key, values]) => [
-			key,
-			Object.entries(values),
-		]),
-	};
+  return {
+    formErrors: Object.entries(state.formErrors).map(([key, values]) => [
+      key,
+      Object.entries(values),
+    ]),
+  };
 }
 
 export default connect(mapStateToProps)(ShowAllErrors);
