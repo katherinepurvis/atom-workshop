61d417fd5cb85c5b40c004bf4efbfeb0b1eedcff
Amy Hughes <amy.hughes@guardian.co.uk>
2019-08-08 18:02:08 +0100

    add node checking to other scripts - these may be run independently and should switch to the correct node version

--- scripts/client-dev.sh
+++ scripts/client-dev.sh
@@ -9,6 +9,20 @@ do
     fi
 done
 
+fileExists() {
+  test -e "$1"
+}
+
+if ! fileExists "$NVM_DIR/nvm.sh"; then
+  node_version=`cat .nvmrc`
+  echo -e "${red}nvm not found ${plain} NVM is required to run this project"
+  echo -e "Install it from https://github.com/creationix/nvm#installation"
+  exit 1
+else
+  source "$NVM_DIR/nvm.sh"
+  nvm install
+fi
+
 export JS_ASSET_HOST="https://atomworkshop-assets.local.dev-gutools.co.uk/assets/"
 
 if [ "$IS_DEBUG" = true ] ; then
